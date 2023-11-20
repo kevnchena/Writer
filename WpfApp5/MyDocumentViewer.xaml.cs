2f2a169e9022c444a7c51cf458b6b41390fe36ff
Kevin <z6256875@gmail.com>
2023-11-20 10:58:24 +0800

    add font settings

--- WpfApp5/MyDocumentViewer.xaml.cs
+++ WpfApp5/MyDocumentViewer.xaml.cs
@@ -19,9 +19,20 @@ namespace WpfApp5
     /// </summary>
     public partial class MyDocumentViewer : Window
     {
+        Color fontColor = Colors.Black;
         public MyDocumentViewer()
         {
             InitializeComponent();
+            fontColorPicker.SelectedColor = fontColor;
+            
+            foreach (FontFamily fontfamily in Fonts.SystemFontFamilies)
+            {
+                fontFamilyComboBox.Items.Add(fontfamily.Source);
+            }
+            fontFamilyComboBox.SelectedIndex= 8;
+            
+            fontSizeComboBox.ItemsSource = new List<double>() { 8, 9, 10, 12, 14, 16, 18, 20, 24, 26, 28, 30, 34, 38, 40, 50, 60, 70, 80 };
+            fontSizeComboBox.SelectedIndex = 3;
         }
         private void New_Excuted(object sender, ExecutedRoutedEventArgs e)
         {
@@ -39,16 +50,50 @@ namespace WpfApp5
 
         private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            object property = rtbEditor.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            object property = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
             boldButton.IsChecked = (property != DependencyProperty.UnsetValue) && property.Equals(FontWeights.Bold);//如果屬性沒有設值和是否粗體
             
-            property = rtbEditor.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            property = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
             italicButton.IsChecked = (property != DependencyProperty.UnsetValue) && property.Equals(FontStyles.Italic);
 
             property = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
             underlineButton.IsChecked = (property != DependencyProperty.UnsetValue) && property.Equals(TextDecorations.Underline);
 
+            //property = rtbEditor.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            //if (property != DependencyProperty.UnsetValue && fontFamilyComboBox.Items.Contains(property.ToString()))
+            //{
+            //    fontFamilyComboBox.SelectedItem = property.ToString();
+            //}
 
+            //property = rtbEditor.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            //if (property != DependencyProperty.UnsetValue)
+            //{
+            //    double fontSize = (double)property;
+            //    fontSizeComboBox.SelectedItem = fontSize;
+            //}
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fontSizeComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontSizeComboBox.SelectedItem);
+            }
+        }
+
+        private void fontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fontFamilyComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void fontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = (Color)e.NewValue;
+            SolidColorBrush fontBrush = new SolidColorBrush(fontColor);
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, fontBrush);
         }
     }
 }
